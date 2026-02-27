e0d8f11f0041eb0cdf51caa48c551f6a7bdf318e
Kohei Aoki <kouheiaoki@gmail.com>
2026-02-28 06:37:34 +0900

    Refactor JUCEture API to expose all public definitions through a single include for better maintainability

--- Source/cpp/JUCEtureAPI.h
+++ Source/cpp/JUCEtureAPI.h
@@ -2,6 +2,30 @@
 
 #include <JuceHeader.h>
 
+// Public API: Drag gesture support
+#include "Drag/IDragHandler.h"
+#include "Drag/IDragMediator.h"
+#include "Drag/DragStartEvent.h"
+#include "Drag/DragMoveEvent.h"
+#include "Drag/DragEndEvent.h"
+
+// Public API: Pinch gesture support
+#include "Pinch/IPinchHandler.h"
+#include "Pinch/IPinchMediator.h"
+#include "Pinch/PinchStartEvent.h"
+#include "Pinch/PinchScaleEvent.h"
+#include "Pinch/PinchEndEvent.h"
+
+// Public API: Single tap support
+#include "SingleTap/ISingleTapHandler.h"
+#include "SingleTap/ISingleTapMediator.h"
+#include "SingleTap/SingleTapEvent.h"
+
+// Public API: Long tap support
+#include "LongTap/ILongTapHandler.h"
+#include "LongTap/ILongTapMediator.h"
+#include "LongTap/LongTapEvent.h"
+
 namespace JUCEtureAPI
 {
 /**
